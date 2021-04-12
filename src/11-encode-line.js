12b7a46d8c7647764b4877a2af709c94842e3ace
NatalliaKri <55781208+NatalliaKri@users.noreply.github.com>
2021-04-12 09:13:25 +0300

    init: implement function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const newArr = [];
+  let counter = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      counter++;
+    }
+    if (str[i] !== str[i + 1]) {
+      if (counter !== 1) {
+        newArr.push(counter);
+      } else newArr.push('');
+      newArr.push(str[i]);
+      counter = 1;
+    }
+  }
+  return newArr.join('');
 }
 
 module.exports = encodeLine;
